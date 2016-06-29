b9e963046fe5373936cca8ef3f7507d1b641e7c5
cickib <bellakocsis06@gmail.com>
2016-06-29 09:51:32 +0200

    person class, normalized numbers, tests OK

--- 3-phone-numbers/main.py
+++ 3-phone-numbers/main.py
@@ -4,23 +4,32 @@ from person import Person
 
 
 def open_csv(file_name):
-    # implent this function
-    pass  # delete this
+    persons = []
+    with open(file_name, "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        for row in reader:
+            persons.append(Person(row[0], Person.normalize_phone_number(row[1])))
+    return persons
 
 
 def get_csv_file_name(argv_list):
-    # implent this function
-    pass  # delete this
+    if len(argv_list) < 2:
+        return None
+    else:
+        return argv_list[1]
 
 
 def format_output(person):
-    # implent this function
-    pass  # delete this
+    if person is None:
+        return 'No match found.'
+    else:
+        return ('This number belongs to: ' + str(person._name))
 
 
 def get_person_by_phone_number(person_list, user_input_phone_number):
-    # implent this function
-    pass  # delete this
+    for i in person_list:
+        if i._phone_number == user_input_phone_number:
+            return i
 
 
 def main():
@@ -32,8 +41,6 @@ def main():
     person_list = open_csv(file_name)
     user_input_phone_number = input('Please enter the phone number: ')
     match_person = get_person_by_phone_number(person_list, user_input_phone_number)
-
     print(format_output(match_person))
-
 if __name__ == '__main__':
     main()
